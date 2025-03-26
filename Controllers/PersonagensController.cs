4c38ea2c8ac4ecf420233fff8da9e7acb5b8c11c
Julia Reis <julyreis.correa@gmail.com>
2025-03-26 19:30:57 -0300

    RpgAPI part-1

---
+++ Controllers/PersonagensController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Models;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonagensController : ControllerBase
+    {
+        
+    }
+}
